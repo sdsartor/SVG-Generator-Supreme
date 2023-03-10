77b8479980db5fbc04a14f34a2bb74cee0ad17df
sdsartor <sdsartor48@gmail.com>
2023-03-10 10:55:12 -0800

    got triangle to work

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const {dot, rectangle, polygon} = require('./lib/shape');
+const fs = require('fs');
+const questions = require('./lib/questions')
 
-class image{
-    constructor(){
-        this.textcontent = ''
-        this.chosenshape = ''
+
+
+function x (data) {
+ return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+ <polygon points="150,25 275,175 25,175" fill="${data.color}" />
+ <text x="50%" y="50%" fill="${data.textColor}" font-size="72" text-anchor="middle" alignment-baseline="central">${data.text}</text>
+ </svg>`
+
+
+}
+
+
+
+function writenewFile(SVG, data) {
+    // fs is necessary to set up the file to be created and README is the 
+    fs.writeFile(SVG, data, function(err) {
+       console.log(SVG)
+       console.log(data)
+       if (err) {
+        return console.log(err)
+       } else {
+    console.log('Way to go, your README file is being generated!')
+       }
+    })
+    
     }
-    render(){
-        return `<svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">`
+    function init() {
+        inquirer.prompt(questions)
+        .then(function(data) {
+            console.log("DATA: ", data)
+            writenewFile("logo.svg", x(data));
+            console.log(data)
+        })
     }
-textcontent(text, colour){
-    this.textcontent= `<text x="150" y="125" font-size="60" fill=${this.colour} text-anchor="middle"`
-}
-chosenshape(shape){
-    this.chosenshape = shape.render()
-}
-}
 
 
-
-const questions = [
-{
-    type: "input",
-    name: "text",
-    message: "Please enter three letter in lowercase for best results.",
-},
-{
-type: "input",
-name: "textColour",
-message: "Please type a color or hexadecimal number for the color of the text." 
-},
-{
-type: "input",
-name: "shape-color",
-message: "Please choose color or hexadecimal number for the color selection of the body of the logo."
-},
-{
-type: "list",
-name: "shape",
-message: "Please choose a chape for your icon.",
-choices: ["Circle", "Square", "Triangle"]
-},
-]
-
+init()
